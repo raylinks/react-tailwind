391b6b80949affacdc3a10841ed1760fa9e68f51
rilwan <olawuyirilwan98@gmail.com>
2020-08-02 17:53:55 +0100

    set up tailwind api done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React , {useState, useEffect }from 'react';
+import React , {useState, useEffect } from 'react';
 import ImageCard  from './components/ImageCard'
+import ImageSearch  from './components/ImageSearch'
 
 function App() {
 
@@ -9,14 +10,17 @@ function App() {
 
     useEffect(() => {
       fetch(``)
-      .then(res => res.json())
+      .then(res => res.json())  
       .then(data =>{ setImages(data.hits);
       setIsLoading(false)
     }).catch(err => console.log(err));
   
-    },[]);
+    },[term]);
   return (
     <div  class="container.mx-auto">
+    <ImageSearch searchText={(text)  => setTerm(text) } />
+    {!isLoading && images.length === 0 && <h1  className="text-6xl text-center mx-auto  mt-32 ">Loding...</h1>}
+
       {isLoading  ? <h1  className="text-6xl text-center mx-auto  mt-32 ">Loding...</h1> : <div className="grid grid-cols-3 gap-4">
       {images.map(image =>(
         <ImageCard key={image.id} image={image} />
