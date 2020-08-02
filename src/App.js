e68066d42a99b8ead2abbd0604362c40f1efcb22
rilwan <olawuyirilwan98@gmail.com>
2020-08-02 15:03:08 +0100

    set up tailwind API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import './App.css';
+import React , {useState, useEffect }from 'react';
+import ImageCard  from './components/ImageCard'
 
 function App() {
-  return (
-    <div>
-    <h1> hello Nigeria</h1>
+
+  const [images, setImages] = useState([]);
+   const [isLoading, setIsLoading] = useState(true);
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+      fetch(``)
+      .then(res => res.json())
+      .then(data =>{ setImages(data.hits);
+      setIsLoading(false)
+    }).catch(err => console.log(err));
   
+    },[]);
+  return (
+    <div  class="container.mx-auto">
+      {isLoading  ? <h1  className="text-6xl text-center mx-auto  mt-32 ">Loding...</h1> : <div className="grid grid-cols-3 gap-4">
+      {images.map(image =>(
+        <ImageCard key={image.id} image={image} />
+      ))}
+      </div>}
     </div>
+
   );
 }
 
-export default App;
+export default App
