0055b0355d70926a4fb2a0dd8ab4391d7bbbff68
rilwan <olawuyirilwan98@gmail.com>
2020-08-02 22:08:44 +0100

    added no images found message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     const [term, setTerm] = useState('');
 
     useEffect(() => {
-      fetch(``)
+      fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())  
       .then(data =>{ setImages(data.hits);
       setIsLoading(false)
@@ -17,9 +17,9 @@ function App() {
   
     },[term]);
   return (
-    <div  class="container.mx-auto">
+    <div  className="container.mx-auto">
     <ImageSearch searchText={(text)  => setTerm(text) } />
-    {!isLoading && images.length === 0 && <h1  className="text-6xl text-center mx-auto  mt-32 ">Loding...</h1>}
+    {!isLoading && images.length === 0 && <h1  className="text-5xl text-center mx-auto  mt-32 ">No Images Found...</h1>}
 
       {isLoading  ? <h1  className="text-6xl text-center mx-auto  mt-32 ">Loding...</h1> : <div className="grid grid-cols-3 gap-4">
       {images.map(image =>(
